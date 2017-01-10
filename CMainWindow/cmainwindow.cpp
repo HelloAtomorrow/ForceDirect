f6d84eaac4ab6d8e07eca51cdc2d4e81deb0dc19
XiaoxiangAi <XiaoxiangAi@XIAOXIANGAI>
2017-01-10 19:35:10 +0800

    整合Qt画图类，但算法仍然不能正确运行。

--- CMainWindow/cmainwindow.cpp
+++ CMainWindow/cmainwindow.cpp
@@ -1,12 +1,88 @@
+#include <QLineEdit>
+#include <QGraphicsView>
+#include <QGraphicsItem>
+#include <QMessageBox>
+#include <string>
 #include "cmainwindow.h"
+#include "CFileOperation.h"
+#include "CForceGuidanceAlgorithm.h"
 
 CMainWindow::CMainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	/*
+	此处创建菜单和按钮
+	*/
+	m_pOpenAction = new QAction(tr("Open"), this);								//创建打开文件Action
+	connect(m_pOpenAction, SIGNAL(triggered()), this, SLOT(TotalOperation()));	//连接事件，调用TotalOperation函数
+	m_pIncreaseAction = new QAction(tr("Increase"), this);						//创建放大Action
+	m_pDecreaseAction = new QAction(tr("Decrease"), this);						//创建缩小Action
+
+	QMenu *pMenuFile = menuBar()->addMenu(tr("&File"));							//创建菜单File
+	pMenuFile->addAction(m_pOpenAction);										//将Open操作添加至菜单
+
+	QMenu *pMenuOperation = menuBar()->addMenu(tr("Operation"));				//创建菜单Operation
+	pMenuOperation->addAction(m_pIncreaseAction);								//将Increase操作添加至菜单
+	pMenuOperation->addAction(m_pDecreaseAction);								//将Decrease操作添加至菜单
+
+	QToolBar *pToolBar = addToolBar(tr("&File"));								//创建工具栏	
+	pToolBar->addAction(m_pOpenAction);											//将Open操作添加至工具栏
+	QLineEdit *pFileAddress = new QLineEdit();									//创建一个输入地址栏
+	pToolBar->addWidget(pFileAddress);
+	pToolBar->addAction(m_pIncreaseAction);
+	pToolBar->addAction(m_pDecreaseAction);
 }
 
 CMainWindow::~CMainWindow()
 {
+	
+}
 
+void CMainWindow::TotalOperation()
+{
+	std::string fileName = "test.txt";
+	CDataManager dataManager;
+	CFileOperation::ReadFile(fileName, dataManager);
+	
+	CForceGuidanceAlgorithm forceGuidance;
+	forceGuidance.AlgorithmCompute(dataManager, 100, 100, 1);
+	
+	QMessageBox::information(NULL, tr("Open"), tr("Open a file"));			//事件操作，弹出窗口。
+	DrawGuidance(dataManager);
+}
+
+bool CMainWindow::DrawGuidance(CDataManager dataManager)
+{
+	const vector<SNode> &nodeData = dataManager.GetNodeData();
+	QGraphicsScene *scene = new QGraphicsScene;
+	QGraphicsView *view = new QGraphicsView(scene);
+	view->setRenderHints(QPainter::Antialiasing);
+	setCentralWidget(view);
+	AddPointItem(scene, nodeData);
+	AddLineItem(scene, nodeData);
+	return true;
+}
+
+void CMainWindow::AddPointItem(QGraphicsScene *scene, const vector<SNode> &nodeData)
+{
+	for (int i = 0; i < nodeData.size(); i++)
+	{
+		QGraphicsEllipseItem *pNodeItem = new QGraphicsEllipseItem(nodeData[i].label_x, nodeData[i].label_y, 10, 10);
+		pNodeItem->setBrush(Qt::red);
+		scene->addItem(pNodeItem);
+	}
+}
+
+void CMainWindow::AddLineItem(QGraphicsScene *scene, const vector<SNode> &nodeData)
+{
+	for (int i = 0; i < nodeData.size(); i++)
+	{
+		for (int j = i; j < nodeData.size(); j += 2)
+		{
+			QGraphicsLineItem *pEdgeItem = new QGraphicsLineItem(nodeData[i].label_x + 5, nodeData[i].label_y + 5, nodeData[j].label_x + 5, nodeData[j].label_y + 5);
+			pEdgeItem->setPen(QPen(Qt::black, 1.5, Qt::SolidLine, Qt::RoundCap));
+			scene->addItem(pEdgeItem);
+		}
+	}
 }
--- CMainWindow/cmainwindow.h
+++ CMainWindow/cmainwindow.h
@@ -1,8 +1,18 @@
+/*
+*此类用于图形化界面
+*设置menu，Toolbar和最终显示节点网络的画布
+*画图采用QGraphics框架
+*/
 #ifndef CMAINWINDOW_H
 #define CMAINWINDOW_H
 
 #include <QtWidgets/QMainWindow>
 #include "ui_cmainwindow.h"
+#include "CDataManager.h"
+#include "SDataStructure.h"
+
+using namespace std;
+
 
 class CMainWindow : public QMainWindow
 {
@@ -12,8 +22,22 @@ public:
 	CMainWindow(QWidget *parent = 0);
 	~CMainWindow();
 
+	bool DrawGuidance(CDataManager dataManager);//画图函数
+
+private slots:
+	void TotalOperation();														//总处理函数,在此函数中创建数据预处理对象，得到vector<vector<string> > totalEdge
+
+private:																				//而后创建算法对象，得到vector<SNode> sNode和vector<SEdge> sEdge，最后调用画图函数。
+	void AddPointItem(QGraphicsScene *scene, const vector<SNode> &nodeData);	//画图过程中添加点的函数
+	void AddLineItem(QGraphicsScene *scene, const vector<SNode> &nodeData);		//画图过程中添加边的函数
+	void IcreaseFigure();														//放大图形的函数
+	void DecreaseFigure();														//缩小图形的函数
+
 private:
 	Ui::CMainWindowClass ui;
+	QAction *m_pOpenAction;			//打开文件Action
+	QAction *m_pIncreaseAction;		//放大Action
+	QAction *m_pDecreaseAction;		//缩小Action
 };
 
 #endif // CMAINWINDOW_H
